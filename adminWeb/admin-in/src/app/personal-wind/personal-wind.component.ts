b5c81ecbf46ce0a0ead379da757e49136fc1f552
eacorsab <emascord@espol.edu.ec>
2022-10-11 01:07:49 -0500

    carpeta angular admin

---
+++ adminWeb/admin-in/src/app/personal-wind/personal-wind.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-wind',
+  templateUrl: './personal-wind.component.html',
+  styleUrls: ['./personal-wind.component.css']
+})
+export class PersonalWindComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
