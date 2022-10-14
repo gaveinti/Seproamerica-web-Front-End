a6ea87332ba2a26c4017022dfb1db1252732f35f
gaveinti <gaveinti>
2022-10-14 01:11:03 -0500

    Mejora de inicio de sesion, pantalla principal y perfil

--- clienteAngular/src/app/app-routing.module.ts
+++ clienteAngular/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
 import { RegistroComponent } from './registro/registro.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: "inicioSesion/:correo", component: InicioSesionComponent},
   { path: "registro", component: RegistroComponent},
-  { path: "principal", component: PrincipalComponent},
+  { path: "principal", component: PrincipalComponent, canActivate: [AuthGuard]},
+  { path: "perfil", component: PerfilComponent},
   //{ path: '',   redirectTo: '/inicioSesion/:correo', pathMatch: 'full'},
   { path: '', component: InicioSesionComponent}
 ];
---
+++ clienteAngular/src/app/auth.guard.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { AuthService } from './services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  //COn este valor cambia (Tengo que ver como obtengo el booleano del otro componente)
+  validarRuta: boolean = true;
+
+  constructor(private aService: AuthService) {}
+
+  /*ngOnInit(){
+    this.aService.currentApprovalStageMessage.subscribe(msg => this.validarRuta = msg)
+    this.submit()
+  }*/
+
+  /*submit()
+  {
+  console.log("Valor de validacion de auth: "+this.validarRuta)
+  this.aService.updateApprovalMessage(this.validarRuta);
+  }*/
+
+  canActivate(
+    /*route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot*/): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      //logica
+    //this.aService.currentApprovalStageMessage.subscribe(msg => this.validarRuta = msg)
+    /*if(this.validarRuta){
+      console.log("entra a habilitar el inicio de sesion")
+      return true
+    }
+    console.log("no habilitar el inicio sesion")
+    console.log("valor de validar ruta:"+this.validarRuta)
+    return false*/
+    console.log("Receptor validacion:" + this.aService.obtenerValidacion())
+    if(this.aService.obtenerValidacion()){
+      return true;
+    } else{
+      console.log("alerta de window")
+      window.alert("No puede iniciar sesion");
+      return false;
+    }
+  }
+
+  
+}
