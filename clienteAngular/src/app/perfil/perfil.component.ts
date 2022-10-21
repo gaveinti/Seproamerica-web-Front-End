646e53dd8a613859e968fad8cc2b6dc2d960a516
gaveinti <gaveinti>
2022-10-21 18:56:25 -0500

    Primera parte para editar datos de usuario

--- clienteAngular/src/app/perfil/perfil.component.ts
+++ clienteAngular/src/app/perfil/perfil.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { RegisterModel } from '../models/register.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClienteWAService } from '../services/cliente-wa.service';
 
 @Component({
   selector: 'app-perfil',
@@ -19,10 +21,43 @@ export class PerfilComponent implements OnInit {
     contrasenia: ''
   };
 
-  constructor(private authService: AuthService) { }
+  constructor(private clienteWAS: ClienteWAService , private authService: AuthService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    //Funcion donde se guardan los datos del usuario que inició sesión
     this.usuario = this.authService.getUsuario();
+  }
+
+  actualizarDatos(): void {
+    //Informacion actualizada a enviar 
+    const usuarioInfoActualizada: RegisterModel = {
+      apellidos: this.usuario.apellidos,
+      nombres: "Eugenio",
+      cedula: this.usuario.cedula,
+      fechaNac: this.usuario.fechaNac,
+      sexo: this.usuario.sexo,
+      correo: this.usuario.correo,
+      telefono: this.usuario.telefono,
+      contrasenia: this.usuario.contrasenia
+    };
+
+    this.clienteWAS.update(this.usuario.correo, usuarioInfoActualizada)
+    .subscribe({//Esto me devuelve el objeto cambiado
+      next: (res) => {
+        console.log("Info actualizada: "+ res);
+        /*Actualizar datos a mostrar en la pagina de perfil*/
+        /*this.usuario.apellidos = res.apellidos*/
+        this.usuario.nombres = res.nombres
+        /*this.usuario.cedula = res.cedula
+        this.usuario.fechaNac = res.fechaNac
+        this.usuario.sexo = res.sexo*/
+        //correo no
+        /*this.usuario.telefono = res.telefono
+        this.usuario.contrasenia = res.contrasenia*/
+
+      },
+      error: (e) => console.log(e)
+    })
   }
 
 }
