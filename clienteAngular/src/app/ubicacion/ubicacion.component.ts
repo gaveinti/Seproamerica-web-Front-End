fa9c1f4fc91ea4ca1902be6e3866f26efe034228
gaveinti <gaveinti>
2022-12-01 22:14:34 -0500

    Agregando mapa para seleccionar localizacion

--- clienteAngular/src/app/ubicacion/ubicacion.component.ts
+++ clienteAngular/src/app/ubicacion/ubicacion.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { RegisterModel } from '../models/register.model';
 import { AuthService } from '../services/auth.service';
 import { FormGroup, FormBuilder, Validators, FormControl, FormControlDirective} from '@angular/forms';
 import { ServicioseleccionadoService } from '../services/servicioseleccionado.service';
 import { FormularioServicio } from '../models/formularioServicio';
 import * as moment from "moment";
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-ubicacion',
   templateUrl: './ubicacion.component.html',
   styleUrls: ['./ubicacion.component.css']
 })
-export class UbicacionComponent implements OnInit {
+export class UbicacionComponent implements AfterViewInit {
+
+  
+
+  private initMap(): void {
+    let map = L.map('map', {
+      center: [ -2.2058400, -79.9079500 ],
+      zoom: 12
+    });
+  
+
+  const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(map);
+  }
 
   usuario: RegisterModel = {
     apellidos: '',
@@ -24,10 +43,17 @@ export class UbicacionComponent implements OnInit {
     contrasenia: ''
   };
 
+  
+
+
+
   constructor(private authService: AuthService, private formBuilder: FormBuilder, private servicioSeleccionadoService: ServicioseleccionadoService) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.usuario = this.authService.getUsuario();
+    this.initMap();
   }
+
+
 
 }
