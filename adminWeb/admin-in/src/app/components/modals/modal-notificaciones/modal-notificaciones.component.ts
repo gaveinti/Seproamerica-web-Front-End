74db14bd4025757524ddefc7b17db84f7561a349
Bryan Loor <brysloor@espol.edu.ec>
2022-12-30 01:24:27 -0500

    Diseño de modal notificaciones

--- adminWeb/admin-in/src/app/components/modals/modal-notificaciones/modal-notificaciones.component.ts
+++ adminWeb/admin-in/src/app/components/modals/modal-notificaciones/modal-notificaciones.component.ts
@@ -9,6 +9,46 @@ export class ModalNotificacionesComponent implements OnInit {
 
   constructor() { }
 
+  notificaciones=[
+    {
+      'titulo':'Nuevo Servicio Solicitado',
+      'texto':'Nuevo servicio solicitaddo',
+      
+    },
+    {
+      'titulo':'Servicio 341 pagado',
+      'texto':'Servicio 341 pagado'
+    },
+    {
+      'titulo':'Nueva notificacion cualquiera',
+      'texto':'Nueva notificacion cualquiera'
+    },
+    {
+      'titulo':'Nuevo Servicio Solicitado',
+      'texto':'Nuevo servicio solicitaddo'
+    },
+    {
+      'titulo':'Nuevo Servicio Solicitado',
+      'texto':'Nuevo servicio solicitaddo'
+    },
+    {
+      'titulo':'Nueva notificacion cualquiera',
+      'texto':'Nueva notificacion cualquiera'
+    },
+    {
+      'titulo':'Servicio 342 pagado',
+      'texto':'Servicio 342 pagado'
+    },
+    {
+      'titulo':'Nuevo Servicio Solicitado',
+      'texto':'Nuevo servicio solicitaddo'
+    },
+    
+
+    
+
+
+  ]
   ngOnInit(): void {
   }
 
